6235e701278b5fa5d3e95bb40b54fd9a15ab1cd9
HIDEHIKO SHIRAIWA <sirap@jcom.home.ne.jp>
2020-01-28 00:00:28 +0900

    0127_show.htmlの画面フロント実装＋サーバサイド

--- app/controllers/flows_controller.rb
+++ app/controllers/flows_controller.rb
@@ -8,8 +8,8 @@ class FlowsController < ApplicationController
   end
   
   def create
-    @flow = Flow.create
-
+    @flow = Flow.create(flow_params)
+    
     if @flow.save 
       flash.now[:alert] = '登録に成功しました。'
       render 'show' #成功の場合
@@ -20,7 +20,13 @@ class FlowsController < ApplicationController
   end     
 
   def show
-
+    @flow = Flow.find(params[:id])
   end 
 
+
+  private
+  def flow_params
+    params.require(:flow).permit(:year_month_day, :income1, :income2, :income3, :income4, :income5, :income6, :income7, :income_total,
+                                 :expense1, :expense2, :expense3, :expense4, :expense5, :expense6, :expense7, :expense_total) 
+  end  
 end
