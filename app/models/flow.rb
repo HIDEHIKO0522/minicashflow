4cdf80218bdaae4700df34dfa7b8b130e1e594e6
HIDEHIKO SHIRAIWA <sirap@jcom.home.ne.jp>
2020-02-04 00:07:25 +0900

    0203_月別集計機能（データ集計）

--- app/models/flow.rb
+++ app/models/flow.rb
@@ -20,13 +20,8 @@ class Flow < ApplicationRecord
     self.income_total = ( income1 + income2 + income3 + income4 + income5 + income6 + income7 )
     self.expense_total = ( expense1 + expense2 + expense3 + expense4 + expense5 + expense6 + expense7 )
     
+    
   end
-
-  # scope :search, -> (search_params) do
-  #   return if search_params.blank?
-  #     year_month_day (search_params[:year_month_day ])
-  #   end 
-  #     scope :year_month_day, -> (year_month_day) { where(year_month_day: year_month_day ) if year_month_day.present? }
 
     # def self.search(search)
     #   if search
