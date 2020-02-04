87a62a7fbaaa2a2680597e09ea0225c91ab96aaa
HIDEHIKO SHIRAIWA <sirap@jcom.home.ne.jp>
2020-02-04 14:16:41 +0900

    0204_追加_データ検索機能
    データの検索を指定日で検索できるように修正

--- app/controllers/flows/searches_controller.rb
+++ app/controllers/flows/searches_controller.rb
@@ -1,10 +1,19 @@
 class Flows::SearchesController < ApplicationController
 
   def index
-    @flows_search = Flow.order("year_month_day DESC").page(params[:page]).per(10)
-  end  
+    @q = Flow.ransack(params[:q])
+    @flow_search = @q.result(distinct: true)
+      unless @flow_search.present?
+        flash.now[:alert] = 'この日付のデータはありません。'
+        redirect_to flows_path    
+      else
+     
+      end  
+  end
 
-
-
+  private
+  def search_params
+    params.require(:q).permit(:year_mpnth_day_eq)
+  end
   
 end
